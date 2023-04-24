624e00885780b44518363d1e914752a5ffaae3d5
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-04-24 09:58:30 +0000

    orders new page

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,6 +2,17 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @customer = current_customer
+  end
+
+  def comfirm
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.shipping_name = current_customer.last_name + current_customer.first_name
+      @order.shipping_address = current_customer.customer_address
+      @order.post_code = current_customer.postal_code
+    elsif params[:order][:select_address] == "1"
+    end
   end
 
   def create
