41880c367cf812ade99f369de33db387426880bd
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-05-07 13:52:44 +0000

    Fix layout

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def show
@@ -10,7 +10,7 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -20,11 +20,11 @@ class Admin::CustomersController < ApplicationController
       render :edit
     end
   end
-  
+
 private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
-  
+
   end
-  
+
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,12 +1,10 @@
 class Admin::HomesController < ApplicationController
   def top
-    @orders = Order.all
+    @orders = Order.page(params[:page])
+    
     #@order.id = @order_detail(:order_id)
 
-
       #@total_quantity += order_detail.quantity
-
-
     #@order.total_quantity = @total_quantity
 
   end
