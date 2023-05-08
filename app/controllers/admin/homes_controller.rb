aaed0a939fadba5b27a111b782ac087cb605ab79
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-05-08 03:08:38 +0000

    Fix layout

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,8 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
   def top
     @orders = Order.page(params[:page])
-    
+
     #@order.id = @order_detail(:order_id)
 
       #@total_quantity += order_detail.quantity
